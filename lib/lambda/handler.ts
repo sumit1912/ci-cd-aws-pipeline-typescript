bd564f52b28ec165bbd79d74857fc58db224987f
sumit1912 <uia77569@contiwan.com>
2023-06-02 16:19:37 +0530

    Changed text in lambda

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -1,7 +1,7 @@
 export async function handler(event: string, context: string) {
     console.log('Stage Name is: ' + process.env.stage);
     return {
-        body: 'Hello from a Lambda Function',
+        body: 'Hakooooonaaaa Mataaataaaa !!!',
         statusCode: 200,
     };
 }
