360e75ef78295aaebe0727c68ad823af89f0d14a
sumit1912 <uia77569@contiwan.com>
2023-06-02 15:08:06 +0530

    Added cdk code

--- lib/ci-cd-aws-pipeline-typescript-stack.ts
+++ lib/ci-cd-aws-pipeline-typescript-stack.ts
@@ -1,5 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+import { pipeline } from 'stream';
+import { MyPipelineAppStage } from './stage';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CiCdAwsPipelineTypescriptStack extends cdk.Stack {
@@ -12,5 +16,24 @@ export class CiCdAwsPipelineTypescriptStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'CiCdAwsPipelineTypescriptQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('sumit1912/ci-cd-aws-pipeline-demo', 'main'),
+        commands: ['npm ci',
+          'npm run build',
+          'npx cdk synth']
+      }),
+    });
+
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: '496932237066', region: 'eu-central-1' }
+    }))
+
+    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+      env: { account: '496932237066', region: 'eu-central-1' }
+    }))
   }
 }
